02a6300a6a5fef8639f0b72c563286748bd387d0
Vitor Gregório <vitorgre23@gmail.com>
2021-01-13 20:35:58 -0300

    Created email queue structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,8 @@ $app = new Laravel\Lumen\Application(
 $app->withFacades();
 
 $app->withEloquent();
+$app->make('queue');
+
 
 /*
 |--------------------------------------------------------------------------
@@ -60,6 +62,17 @@ $app->singleton(
 */
 
 $app->configure('app');
+$app->configure('queue');
+
+$app->configure('mail');
+$app->alias('mailer', Illuminate\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
+
+$app->alias('mail.manager', Illuminate\Mail\MailManager::class);
+$app->alias('mail.manager', Illuminate\Contracts\Mail\Factory::class);
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -92,13 +105,14 @@ $app->routeMiddleware([
 */
 
 
-$app->register(Vinkla\Hashids\HashidsServiceProvider::class);
-
 
 // $app->register(App\Providers\AppServiceProvider::class);
+$app->register(Vinkla\Hashids\HashidsServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Illuminate\Mail\MailServiceProvider::class);
+
 
 
 /*
