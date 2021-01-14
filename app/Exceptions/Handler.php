e4eb168f5956ab8ef5f2fd237fc3c5abb7bb6a6a
Vitor Gregório <vitorgre23@gmail.com>
2021-01-14 18:59:09 -0300

    Add ajustment exceptions handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,13 @@ namespace App\Exceptions;
 
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -20,7 +24,7 @@ class Handler extends ExceptionHandler
         AuthorizationException::class,
         HttpException::class,
         ModelNotFoundException::class,
-        ValidationException::class,
+        ValidationException::class
     ];
 
     /**
@@ -47,8 +51,63 @@ class Handler extends ExceptionHandler
      *
      * @throws \Throwable
      */
-    public function render($request, Throwable $exception)
+    public function render(
+                  $request,
+        Throwable $exception
+    ) {
+        if ($exception instanceof ValidationException) {
+            return $this->handleValidationMessage($exception);
+        }
+
+        if (method_exists($exception, "getCode")) {
+            $code = $exception->getCode();
+        }
+
+        if (method_exists($exception, "getStatusCode")) {
+            $code = $exception->getStatusCode();
+        }
+
+        if ($exception instanceof QueryException) {
+            $code    = Response::HTTP_INTERNAL_SERVER_ERROR;
+            $message = 'Erro ao executar a Query!';
+        }
+
+        if ($exception instanceof NotFoundHttpException || $exception instanceof MethodNotAllowedHttpException) {
+            $code = Response::HTTP_NOT_FOUND;
+            if (null == $message) {
+                $message = trans('Rota não existente!');
+            }
+        }
+
+        if (method_exists($exception, "getMessage")) {
+            $message = $exception->getMessage();
+        }
+
+        $message = $exception->getMessage();
+        if (empty($code)) {
+            $code = 500;
+        }
+        if (empty($message)) {
+            $message = "Erro ao realizar solicitação";
+        }
+
+        return apiResponse($message, $code);
+    }
+
+    private function handleValidationMessage(ValidationException $e)
     {
-        return parent::render($request, $exception);
+        $code          = 400;
+        $errorsMessage = [];
+        $errors        = $e->validator->errors()->toArray();
+
+        foreach ($errors as $field => $message) {
+            $errorsMessage[$field] = $message[0];
+        };
+
+        if (!count($errorsMessage) > 0) {
+            $code = 500;
+        }
+
+        return apiResponse('Erro ao validar os campos', $code, $errorsMessage);
     }
 }
